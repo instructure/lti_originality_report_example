d5086ddbf73100bcb180bc7d5f10f35e8b93091c
wdransfield <wdransfield@instructure.com>
2017-04-20 13:38:00 -0600

    Add Update functionality

--- app/controllers/originality_reports_controller.rb
+++ app/controllers/originality_reports_controller.rb
@@ -1,7 +1,11 @@
 class OriginalityReportsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_filter :verify_authenticity_token, only: %i(create update)
   include OriginalityReportsHelper
 
+  # create
+  #
+  # Creates an originality report in Canvas and persists
+  # an report in the tool if succesfull.
   def create
     head :not_found and return if submission.blank? || assignment.blank?
 
@@ -9,11 +13,30 @@ class OriginalityReportsController < ApplicationController
 
     # Do the originality report create request
     response = HTTParty.post(originality_report_creation_url,
-                             body: { originality_report: originality_report(originality_score) },
+                             body: { originality_report: originality_report_json(originality_score) },
                              headers: authorization_header)
 
     # Store the report if it was created in Canvas
     persist_originality_report(response) if response.code == 201
+
+    render json: response.body, status: response.code
+  end
+
+  # update
+  #
+  # Updates an originality report in Canvas and the tool.
+  def update
+    if submission.blank? || assignment.blank? || originality_report.blank?
+      head :not_found and return
+    end
+
+    response = HTTParty.put(originality_report_edit_url,
+                            body: { originality_report: originality_report_json(params['originality_score']) },
+                            headers: authorization_header)
+
+    if response.code == 200
+      originality_report.update_attributes(originality_score: JSON.parse(response.body)['originality_score'])
+    end
 
     render json: response.body, status: response.code
   end
--- app/helpers/originality_reports_helper.rb
+++ app/helpers/originality_reports_helper.rb
@@ -10,13 +10,19 @@ module OriginalityReportsHelper
     )
   end
 
-  def originality_report(score)
+  def originality_report_json(score)
     {
       originality_report_url: 'http://www.instructure.com',
       # We just grab the last attachment for demoing
       file_id: submission.attachments&.last&.fetch('id'),
       originality_score: score
     }
+  end
+
+  def originality_report_edit_url
+    base_url = tool_proxy_from_assignment.base_tc_url
+    "#{base_url}/api/lti/assignments/#{params['assignment_tc_id']}"\
+    "/submissions/#{params['submission_tc_id']}/originality_report/#{params['or_tc_id']}"
   end
 
   def originality_report_creation_url
@@ -30,5 +36,9 @@ module OriginalityReportsHelper
 
   def assignment
     @_assignment ||= Assignment.find_by(tc_id: params['assignment_tc_id'])
+  end
+
+  def originality_report
+    @_originality_report ||= OriginalityReport.find_by(tc_id: params['or_tc_id'])
   end
 end
