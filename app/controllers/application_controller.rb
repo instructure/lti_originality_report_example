a9600c552adf0c415a148b6b86dd2c8cd5a5d3c6
Nathan Mills <rivernate@users.noreply.github.com>
2017-08-01 08:28:25 -0600

    add method to allow iframes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  private
+  
+  def allow_iframe
+    response.headers.delete "X-Frame-Options"
+  end
+  
 end
