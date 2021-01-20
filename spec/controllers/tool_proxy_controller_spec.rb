b524d07b1a25a3020790aedae1560446dd237ea7
Ryan Hawkins <ryan.hawkins@instructure.com>
2021-01-20 13:26:45 -0700

    Fix Rubocop error
    Fixed the one Rubocop error. *sigh*

--- spec/controllers/tool_proxy_controller_spec.rb
+++ spec/controllers/tool_proxy_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ToolProxyController, type: :controller do
-
   let(:shared_secret) { 'e5bf61debb355e4552732a943c74801ee02bc24ef1d6a077c6e68363fb9dcc4dceab75e6ae4f1e6ae9df5a6892ebbabe49feecc67f00f7e447b43f270e115c590533cd9a176a23eaba334834180da227521884bb49fae1993ca15d52c077b7d37d2ab6fd924a86a285f438f2fc161f63806468d1b977ff120635aa70f9d1d7a5' }
   let(:tool_proxy) { ToolProxy.create!(shared_secret: shared_secret, guid: SecureRandom.uuid, tcp_url: 'test.com', base_url: 'base.com') }
   let(:lti_assignment_id) { SecureRandom.uuid }
